75607e1ad2ce85a5ba7b24e9ef162ac28f330d3d
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2024-01-22 19:05:49 +0530

    update: Choose different ubers

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -8,6 +8,8 @@ import { useDispatch } from 'react-redux';
 import { setDestination } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/native';
 import NavFavourites from './NavFavourites';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from '@rneui/base';
 
 const NavigateCard = () => {
   const dispatch = useDispatch(); 
@@ -42,6 +44,26 @@ const NavigateCard = () => {
           />
         </View>
         <NavFavourites />
+      </View>
+      <View style={tw`flex-row bg-white justify-evenly py-2 at-auto border-top border-gray-100`}>
+        <TouchableOpacity 
+        onPress={() => navigation.navigate("RideOptionsCard")}
+        style={tw`justify-between flex flex-row bg-black w-24 px-4 py-3 rounded-full`}>
+          <Icon
+          name="car" type="font-awesome" color="white" size={16}/>
+          <Text style={tw`text-white text-center`}>
+            Rides
+          </Text>
+
+        </TouchableOpacity>
+        <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+          <Icon
+          name="fast-food-outline" type="ionicon" color="black" size={16}/>
+          <Text style={tw`text-center`}>
+            Eats
+          </Text>
+
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,80 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { FlatList, SafeAreaView, Text, TouchableOpacity ,View ,Image} from 'react-native'
+import React, { useState } from 'react'
+import tw from "twrnc";
+import { Icon } from '@rneui/base';
+import { useNavigation } from '@react-navigation/native';
+
+const data =[
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  }
+];
+
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`flex-grow bg-white`}>
+      <View>
+        <TouchableOpacity 
+        onPress={() => navigation.navigate("NavigateCard")}
+        style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}>
+          <Icon
+          name="chevron-left" type="font-awesome" color="black" size={16}/>
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({item: {id, title, multiplier, image}, item}) => (
+          <TouchableOpacity 
+          onPress={() => setSelected(item)}
+          style={tw`flex-row justify-between items-center px-10 ${id === selected?.id && "bg-gray-200"}`}>
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{uri: image}}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$99</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity 
+        disabled={!selected}
+        style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}>  
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
